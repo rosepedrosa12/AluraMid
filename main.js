5d09d1f4ebd5626df823bf08d701d36793cecc80
Rose Elizangela Martins Pedrosa <115120049+rosepedrosa12@users.noreply.github.com>
2023-10-18 20:19:44 -0300

    dedded

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-function tocaSomPom() {
+function tocaSomPom () {
 document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
+document,querySelector('.tecla_pom').onclick = tocaSomPom;
